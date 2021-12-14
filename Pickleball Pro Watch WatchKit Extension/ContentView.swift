cf0da9556332e869dd6965d427cd790fd67cd908
Eric Romrell <eric.romrell0@walmart.com>
2021-12-14 16:56:32 -0700

    Added watch target

---
+++ Pickleball Pro Watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pickleball Pro Watch WatchKit Extension
+//
+//  Created by Eric Romrell on 12/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pickleball Pro Watch WatchKit Extension/Pickleball_ProApp.swift
@@ -0,0 +1,19 @@
+//
+//  Pickleball_ProApp.swift
+//  Pickleball Pro Watch WatchKit Extension
+//
+//  Created by Eric Romrell on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Pickleball_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
